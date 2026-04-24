ef7cc03ccd138c18f9c27d4f6a42df91eeac75d0
gelgelgelo <asbbibon@gmail.com>
2026-04-25 00:27:15 +0800

    added makefiles for learning

---
+++ arrays/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void)
+{
+	int weeks, days;
+
+	std::cout << "enter number of weeks: ";
+	std::cin >> weeks;
+
+	std::cout << "enter number of days: ";
+	std::cin >> days;
+
+	int arr[weeks][days] = {};
+}
