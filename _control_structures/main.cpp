1e085da74efd749a8ee1b80373e3c736233604b2
gelgelgelo <asbbibon@gmail.com>
2026-04-24 22:44:28 +0800

    completed control structures lab

---
+++ _control_structures/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main(void)
+{
+	double account_balance;
+	std::cout << "enter account balance: ";
+	std::cin >> account_balance;
+
+	do {
+		double withdraw_amount;
+		std::cout << "enter withdrawal amount: ";
+		std::cin >> withdraw_amount;
+
+		if(withdraw_amount == 0000)
+		{
+			std::cout << "Transaction ended" << std::endl;
+			std::cout << "Final Balance is: " << account_balance << std::endl;
+			break;
+		}
+
+		else if(withdraw_amount > account_balance)
+		{
+			std::cout << "Insufficient balance" << std::endl;
+		}
+
+		else if(withdraw_amount < 0)
+		{
+			std::cout << "Invalid amount" << std::endl;
+		}
+		
+		else
+		{
+			account_balance -= withdraw_amount;
+			std::cout << "Current balance: " << account_balance << std::endl;
+			
+			if(account_balance == 0) break;
+
+			else if(withdraw_amount == 0) break;
+		} 
+	} while(true);
+}
+
+
