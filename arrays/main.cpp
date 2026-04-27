1ab46c60473b3eda1eeca09e811aa6d15c8dd1d8
gelgelgelo <asbbibon@gmail.com>
2026-04-27 16:06:57 +0800

    added devel branch

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 int main(void)
 {
-	
+	int day;
+	int week;
+
+	int day_counter = 1;
+	int week_counter = 1;
+
+	cout << "enter number of weeks > ";
+	cin >> week;
+	cout << "enter number of days per week > ";
+	cin >> day;
+
 }
+
