16075360ce80ef50c5ad8ea8d62264d435996ef4
gelgelgelo <asbbibon@gmail.com>
2026-05-10 12:54:49 -0400

    finished functions lab

---
+++ _functions/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int GetGCD(int a, int b)
+{
+	while(a != b)
+	{
+		if(a > b)
+		{
+			a -= b;
+		}
+		else
+		{
+			b -= a;
+		}
+	}	
+	return a;
+}
+
+int GetLCM(int a, int b)
+{
+	return (a * b) / GetGCD(a, b);	
+}
+
+int main()
+{
+	int a, b;
+	std::cout << "Enter the first number: ";
+	std::cin >> a;
+	std::cout << "Enter the second number: ";
+	std::cin >> b;
+	int LCM = GetLCM(a, b);
+	int GCD = GetGCD(a, b);
+
+	std::cout << "GCD of " << a << " and " << b << " is: " << GCD << std::endl;
+	std::cout << "LCM of " << a << " and " << b << " is: " << LCM << std::endl;
+	std::cout << "Common Divisors of " << a << " and " << b << " is: ";
+	for(int i = 1; i < GCD + 1; i++)
+	{
+		if(GCD % i == 0) 
+		{
+			std::cout << i << ",";
+		}
+	}
+	std::cout << std::endl;
+}
+
