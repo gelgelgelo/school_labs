5b1b30be156d0da97c627f5e025a069c3ab24f80
gelgelgelo <asbbibon@gmail.com>
2026-05-13 18:00:25 -0400

    add err handling

--- _functions/main.cpp
+++ _functions/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <climits>
+#include <limits>
 
 int GetGCD(int a, int b)
 {
@@ -20,14 +22,25 @@ int GetLCM(int a, int b)
 int main()
 {
 	int a, b;
-	std::cout << "Enter the first number: ";
-	std::cin >> a;
-	std::cout << "Enter the second number: ";
-	std::cin >> b;
-	if(a < 0 || b < 0) 
+	bool isValid = 0;
+	while(!isValid)
 	{
-		std::cout << "err: negative number detected.\n"; 
-		return 0;
+		std::cout << "Enter the first number: ";
+		std::cin >> a;
+		std::cout << "Enter the second number: ";
+		std::cin >> b;
+		if(std::cin.fail() || a < 0 || b < 0) 
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "err: invalid input detected.\n"; 
+			continue;
+		}
+		
+		else
+		{
+			isValid = 1;
+		}
 	}
 	int LCM = GetLCM(a, b);
 	int GCD = GetGCD(a, b);
@@ -52,4 +65,3 @@ int main()
 	}
 	std::cout << std::endl;
 }
-
